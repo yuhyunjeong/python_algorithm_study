bf1f4736b02756e441f1d2f544642168dea0a5fa
sunjung <sunj20037@gmail.com>
2022-07-02 23:20:45 +0900

    0702

---
+++ sunjung/0702/0702_14681.py
@@ -0,0 +1,13 @@
+import sys
+
+X = int(input())
+Y = int(input())
+
+if X>0 and Y>0 : 
+    print('1')
+elif X<0 and Y>0 : 
+    print('2')
+elif X<0 and Y<0 : 
+    print('3')
+elif X>0 and Y<0 : 
+    print('4')
