264756c7ba9211e50a3e83c7d0f275da4fb8ad7a
yuhyun <jadejung95@gmail.com>
2022-07-08 20:35:27 +0900

    0708_CLASS 1

---
+++ yuhyun/CLASS 1/0708_1152.py
@@ -0,0 +1,4 @@
+str = list(input().split())
+# str = input().split() # 리스트
+
+print(len(str))
---
+++ yuhyun/CLASS 1/0708_2577.py
@@ -0,0 +1,18 @@
+
+
+num = [int(input()) for _ in range(3)]
+result = list(str(num[0] * num[1] * num[2]))
+
+for i in range(10):
+    print(result.count(str(i)))
+
+# 풀이 2
+multiple = 1
+for _ in range(3):
+    multiple *= int(input())
+
+result = list(str(multiple))
+
+for i in range(10):
+    print(result.count(str(i)))
+
