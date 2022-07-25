4d750fdc3d06a4bb7868fe38ced14dcc9ad0ca3c
yuhyun <jadejung95@gmail.com>
2022-07-25 21:04:46 +0900

    programmers

---
+++ yuhyun/Programmers/mock_exam_3.py
@@ -0,0 +1,17 @@
+# 스택
+
+def solution(order):
+    answer = 0
+
+    list = [1,2,3,4,5]
+    sub = []
+    
+    for i in order:
+        for j in list:
+            if list[j]==order[i]:
+                break
+            list.remove(list[j])
+            sub.append(list[j])
+
+
+    return answer
