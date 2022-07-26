baf7a271579375f729edd084f32a2c9746e72362
jonghwee <pjh52682428@gmail.com>
2022-07-26 16:25:26 +0900

    0726_6009~6018

---
+++ jonghwee/0726/codeup_6009-6018.py
@@ -0,0 +1,24 @@
+import sys
+
+# c = sys.stdin.readline().rstrip();
+# print(c);
+
+# a = int(sys.stdin.readline().rstrip());
+# print(a);
+
+# f = float(sys.stdin.readline().rstrip());
+# print(f);
+
+# b = int(sys.stdin.readline().rstrip());
+# print(b);
+
+# d, e = map(sys.stdin.readline().split());
+# print(d);
+# print(e);
+
+# d,e = sys.stdin.readline().split();
+# e = sys.stdin.readline().rstrip();
+# print(e,d);
+
+a,b = sys.stdin.readline().split(":");
+print(a,b,sep=":");
