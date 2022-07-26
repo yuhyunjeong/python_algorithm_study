95f11304e42f2cdfea765eb2b7025303d72db3ac
jonghwee <pjh52682428@gmail.com>
2022-07-26 23:37:35 +0900

    0726_codeup 6019~2021

---
+++ jonghwee/0726/codeup_6019.py
@@ -0,0 +1,17 @@
+import sys
+
+# 6019
+# a,b,c = sys.stdin.readline().rstrip().split(".");
+# print(c,b,a,sep="-");
+
+# 6020
+# a, b = sys.stdin.readline().rstrip().split('-');
+# print(a,b,sep='');
+
+# 6021
+# a = sys.stdin.readline().rstrip();
+# for i in range(0,len(a)):
+#     print(a[i])
+
+# 6022
+a = sys.stdin.readline().rstrip();
