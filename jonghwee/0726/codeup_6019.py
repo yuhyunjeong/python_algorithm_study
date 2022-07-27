f15b669e031e437df36668238682432cc24007c7
jonghwee <pjh52682428@gmail.com>
2022-07-27 23:45:51 +0900

    0727_codeup6019~6023

--- jonghwee/0726/codeup_6019.py
+++ jonghwee/0726/codeup_6019.py
@@ -14,4 +14,13 @@ import sys
 #     print(a[i])
 
 # 6022
-a = sys.stdin.readline().rstrip();
+# a = sys.stdin.readline().rstrip();
+# for i in range(0,len(a)-1,2):
+#     print(a[i:i+2]);
+
+# 6023
+# a,b,c = sys.stdin.readline().rstrip().split(':');
+# print(b);
+
+# a = sys.stdin.readline().rstrip();
+# print(a[4:5]);
