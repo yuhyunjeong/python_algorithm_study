9166cb6cb15e2ff661227ff3ee55804dbbd364c4
jonghwee <pjh52682428@gmail.com>
2022-07-28 20:26:40 +0900

    0728_6019~6038

--- jonghwee/0726/codeup_6019.py
+++ jonghwee/0726/codeup_6019.py
@@ -16,7 +16,7 @@ import sys
 # 6022
 # a = sys.stdin.readline().rstrip();
 # for i in range(0,len(a)-1,2):
-#     print(a[i:i+2]);
+#     print(a[i:i+2], end=" ");
 
 # 6023
 # a,b,c = sys.stdin.readline().rstrip().split(':');
@@ -45,3 +45,44 @@ import sys
 # 6028
 # a = int(sys.stdin.readline().rstrip());
 # print('%X'% a);
+
+# 6029
+# a = int(sys.stdin.readline().rstrip(),16);
+# print('%o'% a);
+
+# # 6030
+# a = sys.stdin.readline().rstrip();
+# print(ord(a));
+
+# 6031
+# a = int(sys.stdin.readline().rstrip());
+# print(chr(a));
+
+# 6032
+# a = int(sys.stdin.readline().rstrip());
+# print(-a);
+
+# 6033
+# a = ord(sys.stdin.readline().rstrip());
+# print(chr(a+1));
+
+# 6034
+# a,b = map(int,sys.stdin.readline().rstrip().split());
+# print(a-b);
+
+# 6035
+# a,b = map(float,sys.stdin.readline().rstrip().split());
+# print(a*b);
+
+# 6036
+# a,b = sys.stdin.readline().rstrip().split();
+# print(a*int(b));
+
+# 6037
+# a= sys.stdin.readline().rstrip();
+# b= sys.stdin.readline().rstrip();
+# print(int(a)*b);
+
+# 6038
+# a,b = map(int,sys.stdin.readline().rstrip().split());
+# print(a**b);
