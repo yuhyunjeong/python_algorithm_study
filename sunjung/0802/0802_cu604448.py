0b33a77402c1df7ed647ce0f614ea3ef2fdf2d26
sunjung <sunj20037@gmail.com>
2022-08-02 20:37:17 +0900

    0802 Python 기초 100제

---
+++ sunjung/0802/0802_cu604448.py
@@ -0,0 +1,29 @@
+#6044
+""" a,b = map(int,input().split(' '))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(format(a/b,".2f")) """
+
+#6045
+""" a, b, c = map(int,input().split())
+print(a+b+c,format((a+b+c)/3,".2f")) """
+
+#6046
+""" a = int(input())
+print(a*2) """
+
+#6047
+""" a, b = map(int, input().split())
+c = 2**b
+print(a*c) """
+
+#6048
+a, b = map(int, input().split())
+
+if (a < b):
+    print("True")
+else:
+    print("False")
---
+++ sunjung/0802/0802_cu604953.py
@@ -0,0 +1,38 @@
+#6049
+""" a, b = map(int, input().split())
+if (a == b):
+    print("True")
+else:
+    print("False") """
+
+#6050
+""" a, b = map(int, input().split())
+
+if (a <= b):
+    print("True")
+else:
+    print("False") """
+
+#6051
+""" a, b = map(int, input().split())
+
+if (a != b):
+    print("True")
+else:
+    print("False") """
+
+#6052
+""" a = int(input())
+
+if (a == 0):
+    print("False")
+else:
+    print("True") """
+
+#6053
+a = bool(int(input()))
+
+if (a == True):
+    print("False")
+else:
+    print("True")
