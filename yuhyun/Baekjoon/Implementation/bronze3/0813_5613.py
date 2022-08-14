700bf181092fbe2162a50fbe24b0358156e4344f
yuhyun <jadejung95@gmail.com>
2022-08-14 20:53:27 +0900

    0814

--- yuhyun/Baekjoon/Implementation/bronze3/0813_5613.py
+++ yuhyun/Baekjoon/Implementation/bronze3/0813_5613.py
@@ -2,12 +2,11 @@ result = int(input())
 while True:
     cal = input()
     
-    
-   
     if cal == '=':
         break
+
     num = int(input())
-    
+
     if cal == '+':
         result = result + num
     elif cal == '-':
@@ -15,6 +14,6 @@ while True:
     elif cal == '*':
         result = result * num
     elif cal == '/':
-        result = result / num
+        result = result // num # 소수점 버림
 
 print(result)
