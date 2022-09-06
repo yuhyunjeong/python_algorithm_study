f6286a91201d6fc68119667185d88da3b7f05cf3
yuhyun <jadejung95@gmail.com>
2022-09-06 23:30:44 +0900

    0906_막대기

--- yuhyun/Baekjoon/Data structures/0906_17608.py
+++ yuhyun/Baekjoon/Data structures/0906_17608.py
@@ -1,14 +1,17 @@
-n = int(input())
+import sys
+
+
+n = int(sys.stdin.readline()) # 시간초과 방지
 lst = []
 
 for i in range(n):
-    h = int(input())
+    h = int(sys.stdin.readline())
     lst.append(h)
 
 last = lst[-1]
 cnt = 1
 
-for i in reversed(range(n)): # for문 거꾸로 반복
+for i in range(n-1,-1,-1): # for문 거꾸로 반복 / for i in reversed(range(n)) 은 시간초과
     if lst[i] > last:
         cnt += 1
         last = lst[i] # 비교할 숫자 갱신
