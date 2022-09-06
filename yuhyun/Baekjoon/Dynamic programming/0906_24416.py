0b17a8f029492e26f4723d3d8dc2bd335784a568
yuhyun <jadejung95@gmail.com>
2022-09-06 20:47:48 +0900

    풀이2

--- yuhyun/Baekjoon/Dynamic programming/0906_24416.py
+++ yuhyun/Baekjoon/Dynamic programming/0906_24416.py
@@ -28,3 +28,28 @@ def fibonacci(n) :
 # }
 
 print(fib(n),fibonacci(n))
+
+# 풀이 2
+
+n = int(input())
+ans = [0, 1]
+dp_li = [0, 1, 1]
+
+def recur(n):
+    global ans
+    if n < 3:
+        ans[0] += 1
+        return 1
+    else: return recur(n - 1) + recur(n - 2)
+
+def dp(n):
+    global ans, dp_li
+    if n < 3: return dp_li[n]
+    for i in range(3, n):
+        ans[1] += 1
+        dp_li.append(dp_li[i - 1] + dp_li[i - 2])
+    return dp_li[-1]
+
+recur(n)
+dp(n)
+print(*ans)
