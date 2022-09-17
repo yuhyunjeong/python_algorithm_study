95cfc08c5077b7d88cd330286298a239cdc68397
yuda <yudajgim@gmail.com>
2022-09-18 07:09:59 +0900

    0918 구현

---
+++ yuda/Implementation/0918_23810.py
@@ -0,0 +1,9 @@
+n = int(input())
+for _ in range(n):
+    print("@" * 5 * n)
+for _ in range(n):
+    print("@" * n)
+for _ in range(n):
+    print("@" * 5 * n)
+for _ in range(2 * n):
+    print("@" * n)
